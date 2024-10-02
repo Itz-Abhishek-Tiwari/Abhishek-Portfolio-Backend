def0b5393a4540c3cd9405369773d7e409313b2a
Abhishek Hyprland <abhitiwariabhi5@gmail.com>
2024-10-02 23:34:53 +0530

    Deploying Django backend to Vercel

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -59,6 +59,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://abhishek-portfolio-backend-lac.vercel.app",
 ]
 
 ROOT_URLCONF = "portfolio.urls"
